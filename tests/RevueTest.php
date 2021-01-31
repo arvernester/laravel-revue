c74c93d32d3c6ef2b776e73f0d0966c572aa7f12
Yugo <dedy.yugo.purwanto@gmail.com>
2021-01-31 17:34:48 +0700

    Test for me()

--- tests/RevueTest.php
+++ tests/RevueTest.php
@@ -2,10 +2,14 @@
 
 namespace Yugo\Revue\Tests;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
 use Yugo\Revue\ServiceProvider;
 use Orchestra\Testbench\TestCase;
-use Yugo\Revue\Facades\Revue as FacadesRevue;
-use Yugo\Revue\Revue;
+use Yugo\Revue\Facades\Revue;
+use Yugo\Revue\Service;
 
 class RevueTest extends TestCase
 {
@@ -19,5 +23,26 @@ class RevueTest extends TestCase
         return [
             'revue' => Revue::class,
         ];
+    }
+
+    private function mockHttp(array $response): Client
+    {
+        $stack = HandlerStack::create(new MockHandler($response));
+        
+        return new Client(['handler' => $stack]);
+    }
+
+    public function testMe(): void
+    {
+        $expectation = ['profile_id' => 'https://www.getrevue.co/profile/getrevue'];
+        $http = $this->mockHttp([new Response(200, [], json_encode($expectation))]);
+
+        $response = (new Service($http))->request('accounts/me');
+
+        Revue::shouldReceive('me')
+            ->once()
+            ->andReturn($expectation);
+
+        $this->assertSame(Revue::me(), json_decode($response, true));
     }
 }
